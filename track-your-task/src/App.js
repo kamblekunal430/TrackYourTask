99bd5ed7a9b67840b18368a733a9f38e00861ac8
kamblekunal430 <kamblekunal430@gmail.com>
2021-08-21 13:34:25 +0530

    created todo add functionality

--- track-your-task/src/App.js
+++ track-your-task/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { TodoAdd } from "./components/TodoAdd";
 import { Todos } from "./components/Todos";
 import { Navbar } from "./Navbar";
 
@@ -11,6 +12,7 @@ class App extends Component {
         <Navbar />
         <br />
         <div className="container">
+          <TodoAdd />
           <Route path="/" exact component={Todos} />
           <Route path="/todos" component={Todos} />
         </div>
---
+++ track-your-task/src/components/TodoAdd.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export class TodoAdd extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null,
+      status: "incomplete",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const todo = {
+      name: this.state.name,
+      status: this.state.status,
+    };
+    axios
+      .post("http://localhost:5001/todos", todo)
+      .then((result) => {
+        alert("Todo added");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    //console.log(event.target.value);
+    this.setState({ [name]: value });
+    //console.log(this.state);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="mb-2">
+          <label className="form-label">
+            <b>
+              <h3>New Todo:</h3>
+            </b>
+          </label>
+          <div className="d-flex col-md-8">
+            <input
+              type="text"
+              name="name"
+              className="form-control bg-light"
+              placeholder="Add Task Here..."
+              onChange={this.handleChange}
+            />
+            &emsp;
+            <button type="submit" className="btn btn-primary">
+              ADD
+            </button>
+            &emsp;
+            <input type="reset" value="Clear" className="btn btn-danger" />
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
